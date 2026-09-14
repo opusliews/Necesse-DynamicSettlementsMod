ec9b5a9b5588caffda8dc34e31e8e8cd4ee616ab
Opus <opusliews@gmail.com>
2026-09-14 17:18:40 -0300

    Crafting Rework: timed anvil crafting

---
+++ src/main/java/opusliews/patches/IronAnvilContainerOpenPatch.java
@@ -0,0 +1,28 @@
+package opusliews.patches;
+
+import necesse.engine.modLoader.annotations.ModMethodPatch;
+import necesse.engine.network.server.ServerClient;
+import necesse.inventory.container.object.CraftingStationContainer;
+import necesse.level.gameObject.container.IronAnvilObject;
+import necesse.level.maps.Level;
+import net.bytebuddy.asm.Advice;
+import opusliews.crafting.IronAnvilFeature;
+
+@ModMethodPatch(
+		target = CraftingStationContainer.class,
+		name = "openAndSendContainer",
+		arguments = {int.class, ServerClient.class, Level.class, int.class, int.class}
+)
+public class IronAnvilContainerOpenPatch {
+	@Advice.OnMethodEnter
+	public static void onEnter(
+			@Advice.Argument(value = 0, readOnly = false) int containerID,
+			@Advice.Argument(2) Level level,
+			@Advice.Argument(3) int tileX,
+			@Advice.Argument(4) int tileY
+	) {
+		if (level.getObject(tileX, tileY) instanceof IronAnvilObject) {
+			containerID = IronAnvilFeature.ironAnvilContainerID;
+		}
+	}
+}
