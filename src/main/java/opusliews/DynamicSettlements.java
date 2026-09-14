dabdda43982cb5cff8f4621443a1ceb6f79a5fe0
Opus <opusliews@gmail.com>
2026-09-14 00:13:29 -0300

    Added (in)compatibility patch

--- src/main/java/opusliews/DynamicSettlements.java
+++ src/main/java/opusliews/DynamicSettlements.java
@@ -1,10 +1,22 @@
 package opusliews;
 
+import necesse.engine.loading.ClientLoader;
 import necesse.engine.localization.message.LocalMessage;
+import necesse.engine.localization.message.StaticMessage;
+import necesse.engine.modLoader.LoadedMod;
+import necesse.engine.modLoader.ModListData;
+import necesse.engine.modLoader.ModLoader;
 import necesse.engine.modLoader.annotations.ModEntry;
 import necesse.engine.network.PacketReader;
 import necesse.engine.registries.*;
+import necesse.engine.window.GameWindow;
+import necesse.engine.window.WindowManager;
 import necesse.entity.mobs.job.JobType;
+import necesse.gfx.forms.components.localComponents.FormLocalLabel;
+import necesse.gfx.forms.components.localComponents.FormLocalTextButton;
+import necesse.gfx.forms.presets.ContinueForm;
+import necesse.gfx.forms.presets.ModsForm;
+import necesse.gfx.gameFont.FontOptions;
 import necesse.inventory.recipe.Ingredient;
 import necesse.inventory.recipe.Recipe;
 import necesse.inventory.recipe.Recipes;
@@ -33,12 +45,116 @@ import opusliews.settler.BuilderRequestLevelData;
 import opusliews.settler.BuilderSettler;
 import opusliews.sleep.SettlementSleepSettingsLevelData;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @ModEntry
 public class DynamicSettlements {
 	public static int blueprintWorkstationContainerID;
 	public static boolean debugBlueprintMaterialGrant = false;
+	public static boolean SBCompatFailure = false;
+
+	public void preInit() {
+		boolean settlementBuildersLoaded = ModLoader.getEnabledMods().stream()
+				.anyMatch(mod -> mod.id.equals("opusliews.settlementbuilders"));
+
+		if (!settlementBuildersLoaded) {
+			return;
+		}
+
+		SBCompatFailure = true;
+
+		setupIncompatibilityNotice();
+	}
+
+	private void setupIncompatibilityNotice() {
+		ContinueForm form = new ContinueForm(
+				"dynamicsettlementsincompatibility",
+				520,
+				300
+		) {
+			@Override
+			public void onWindowResized(GameWindow window) {
+				super.onWindowResized(window);
+				setPosMiddle(window.getHudWidth() / 2, window.getHudHeight() / 2);
+			}
+
+			@Override
+			public boolean canContinue() {
+				return false;
+			}
+		};
+
+		form.addComponent(new FormLocalLabel(
+				new StaticMessage(
+						"\"Dynamic Settlements\" cannot be loaded alongside \"Settlement Builders\".\n\n"
+								+ "\"Dynamic Settlements\" already includes all \"Settlement Builders\" features.\n\n"
+								+ "Choose which mod you want to disable:"
+				),
+				new FontOptions(20),
+				0,
+				form.getWidth() / 2,
+				20,
+				form.getWidth() - 30
+		));
+
+		FormLocalTextButton disableSettlementBuilders = form.addComponent(
+				new FormLocalTextButton(
+						new StaticMessage("Disable \"Settlement Builders\" and restart"),
+						20,
+						175,
+						form.getWidth() - 40
+				)
+		);
+
+		disableSettlementBuilders.onClicked(event -> {
+			disableModAndRestart("opusliews.settlementbuilders");
+		});
+
+		FormLocalTextButton disableDynamicSettlements = form.addComponent(
+				new FormLocalTextButton(
+						new StaticMessage("Disable \"Dynamic Settlements\" and restart"),
+						20,
+						225,
+						form.getWidth() - 40
+				)
+		);
+
+		disableDynamicSettlements.onClicked(event -> {
+			disableModAndRestart("opusliews.dynamicsettlements");
+		});
+
+		ClientLoader.loadingNoticeForms.add(form);
+	}
+
+	private void disableModAndRestart(String modID) {
+		List<ModListData> modList = new ArrayList<>();
+
+		for (LoadedMod mod : ModLoader.getAllMods()) {
+			ModListData data = new ModListData(mod);
+
+			if (mod.id.equals(modID)) {
+				data.enabled = false;
+			}
+
+			modList.add(data);
+		}
+
+		ModLoader.saveModListSettings(modList);
+
+		Runnable restart = ModsForm.restartGameRunnable();
+
+		if (restart != null) {
+			restart.run();
+		} else {
+			WindowManager.getWindow().requestClose();
+		}
+	}
 
 	public void init() {
+		if (SBCompatFailure) {
+			return;
+		}
 		// Registrations
 		BuffRegistry.registerBuff(MalignanceGogglesItem.buffStringID, new MalignanceGogglesBuff());
 		SettlerRegistry.registerSettler("builder", new BuilderSettler());
@@ -139,6 +255,9 @@ public class DynamicSettlements {
 	}
 
 	public void postInit() {
+		if (SBCompatFailure) {
+			return;
+		}
 		Recipes.registerModRecipe(new Recipe(
 				"blueprintItem",
 				1,
